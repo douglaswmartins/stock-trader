2d4f31f1a7191870626b34230d731d11490cad38
Douglas Martins <martins.douglas1997@outlook.com>
2021-06-10 18:31:57 -0300

    Adicionado animação nas transições

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
 
     <v-main>
       <v-container>
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
       </v-container>
     </v-main>
   </v-app>
@@ -23,5 +25,21 @@ export default {
 </script>
 
 <style scoped>
+  @keyframes slide-in {
+    from { transform: translateY(-30px); opacity: 0; }
+    to { transform: translateY(0px); opacity: 1; }
+  }
 
+  @keyframes slide-out {
+    form { transform: translateY(0px); opacity: 1; }
+    to { transform: translateY(-30px); opacity: 0; }
+  }
+
+  .slide-enter-active {
+    animation: slide-in 300ms ease;
+  }
+
+  .slide-leave-active {
+    animation: slide-out 300ms ease;
+  }
 </style>
